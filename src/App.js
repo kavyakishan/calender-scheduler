eeec0524ac265c85e36855585f844b5ee27c46da
kavyakishan <kavya.cuty99@gmail.com>
2020-06-29 23:31:48 +0530

    Calender scheduler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import { Day, Week, Month, Agenda, ScheduleComponent, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { extend } from '@syncfusion/ej2-base';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const data = extend([], localStorage.getItem('data') === undefined ? [] : JSON.parse(localStorage.getItem('data')), null, true);
+  const scheduleObj = useRef();
+  const resourceData = [
+      { text: 'Kavya', id: 1, color: '#1aaa55' },
+      { text: 'Vivek', id: 2, color: '#7fa900' },
+      { text: 'Deepika', id: 3, color: '#87ceeb' },
+  ];
+
+const onActionComplete = (args) => {
+  localStorage.setItem('data',JSON.stringify(scheduleObj.current.dataModule.dataManager.dataSource.json));
+}
+
+return (
+<div className='schedule-control-section'>
+  <div className='col-lg-12 control-section'>
+    <div className='control-wrapper'>
+      <ScheduleComponent 
+      width='100%' 
+      height='650px' 
+      eventSettings={{
+        dataSource: data, 
+        fields: {
+            subject: { title: 'Task', name: 'Subject' },
+            location: { title: 'Project Name', name: 'Location' },
+            description: { title: 'Comments', name: 'Description' }
+        }
+      }}
+      ref={(ref) => {
+        scheduleObj.current = ref;
+      }}
+      actionComplete={onActionComplete} 
+      group={{ byDate: true, resources: ['Owners'] }}>
+        <ResourcesDirective>
+          <ResourceDirective 
+            field='TaskId' 
+            title='Assignee' 
+            name='Owners' 
+            allowMultiple={true} 
+            dataSource={resourceData} 
+            textField='text' 
+            idField='id'
+            colorField='color'>
+          </ResourceDirective>
+        </ResourcesDirective>
+        <ViewsDirective>
+          <ViewDirective option='Day'/>
+          <ViewDirective option='Week'/>
+          <ViewDirective option='Month'/>
+          <ViewDirective option='Agenda'/>
+        </ViewsDirective>
+        <Inject services={[Day, Week, Month, Agenda, Resize, DragAndDrop]}/>
+      </ScheduleComponent>
     </div>
-  );
+  </div>
+</div>);
 }
 
 export default App;
